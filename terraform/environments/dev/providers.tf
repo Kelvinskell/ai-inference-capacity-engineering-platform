a1de5cc355c9464b99e0a3b33df0f0a4aa3b6f82
Kelvin Onuchukwu <kelvin.onuchukwu@capgemini.com>
2026-08-12 08:29:09 +0200

    fix versioning constraints

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -5,3 +5,13 @@ provider "aws" {
     tags = var.tags
   }
 }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
