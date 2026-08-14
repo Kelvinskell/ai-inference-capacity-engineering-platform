a1e18a7c440a8d97e86598240dda4d0a8494c311
Kelvin Onuchukwu <kelvin.onuchukwu@capgemini.com>
2026-08-14 07:45:40 +0200

    chnage ns name

--- terraform/modules/s3-model-storage/pod-identity-association.tf
+++ terraform/modules/s3-model-storage/pod-identity-association.tf
@@ -1,7 +1,7 @@
 # Pod identity assoc for model uploader
 resource "aws_eks_pod_identity_association" "model_uploader" {
   cluster_name    = var.eks_cluster_name
-  namespace       = "model-uploader"
+  namespace       = "model-storage"
   service_account = "model-uploader"
   role_arn        = aws_iam_role.model_uploader.arn
 }
